c21621e01b24d754812590c629055364430b283b
Lar089 <carolinelara35@gmail.com>
2021-05-27 16:33:21 -0300

    Add project files.

---
+++ PeriodoAtrasTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PeriodoAtrasTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PeriodoAtrasTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e737af14-d457-4712-b99f-0863a5973137")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PeriodoAtrasTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PeriodoAtrasTest.Periodo;
+
+namespace PeriodoAtrasTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UmDiasAtras()
+        {
+            Assert.AreEqual("um dia", new Humunizar(DateTime.Parse("26/05/2021"), DateTime.Parse("25/05/2021")).valor_saida);
+        }
+
+        [TestMethod]
+        public void DoisDiasAtras()
+        {
+            Assert.AreEqual("dois dias", new Humunizar(DateTime.Parse("26/05/2021"), DateTime.Parse("24/05/2021")).valor_saida);
+        }
+
+        [TestMethod]
+        public void UmaSemanaAtras()
+        {
+            Assert.AreEqual("uma semana", new Humunizar(DateTime.Parse("26/05/2021"), DateTime.Parse("19/05/2021")).valor_saida);
+        }
+
+        [TestMethod]
+        public void DoisMesesUmaSemanaAtras()
+        {
+            Assert.AreEqual("dois meses e uma semana", new Humunizar(DateTime.Parse("26/05/2021"), DateTime.Parse("19/03/2021")).valor_saida);
+        }
+
+        [TestMethod]
+        public void DoisMesesDoisDiasAtras()
+        {
+            Assert.AreEqual("dois meses e dois dias", new Humunizar(DateTime.Parse("26/05/2021"), DateTime.Parse("24/03/2021")).valor_saida);
+        }
+
+        [TestMethod]
+        public void DezAnosAtras()
+        {
+            Assert.AreEqual("dez anos", new Humunizar(DateTime.Parse("26/05/2021"), DateTime.Parse("26/05/2011")).valor_saida);
+        }
+    }
+}
